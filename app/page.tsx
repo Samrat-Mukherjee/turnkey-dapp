c2784d24133820eb5d9c8a190f1ab5b8388b11db
Samrat-Mukherjee <samrat.mukherjee2022@gmail.com>
2025-08-04 17:04:50 +0530

    passkey, email auth added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+"use client";
 import { Auth } from "@turnkey/sdk-react";
-import Image from "next/image";
 
 export default function Home() {
-  // The auth methods to display in the UI
-  const config: any = {
+  type Config = {
+    authConfig: AuthConfig;
+    configOrder: ConfigOrder[];
+  };
+  type AuthConfig = {
+    emailEnabled: boolean;
+    passkeyEnabled: boolean;
+    phoneEnabled: boolean;
+    appleEnabled: boolean;
+    facebookEnabled: boolean;
+    googleEnabled: boolean;
+    walletEnabled: boolean;
+  };
+
+  type ConfigOrder = "email" | "passkey" | "phone" | "socials";
+
+  const config: Config = {
     authConfig: {
       emailEnabled: true,
       passkeyEnabled: true,
@@ -14,10 +29,22 @@ export default function Home() {
       walletEnabled: false,
     },
     // The order of the auth methods to display in the UI
-    configOrder: ["email", "passkey" /*  "phone", "socials" */],
+    configOrder: ["email", "passkey" /*  "phone" , "socials" */],
   };
-  return <div>
 
-    <Auth {...config}/>
-  </div>;
+  const onAuthSuccess = async () => {
+    console.log("✅ Auth Success:");
+  };
+  const onError = (err: any) => {
+    console.error("❌ Auth Error:", err);
+  };
+  return (
+    <div>
+      <Auth
+        {...config}
+        onAuthSuccess={onAuthSuccess}
+        onError={(e) => onError(e)}
+      />
+    </div>
+  );
 }
